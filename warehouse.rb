65206a37724e50ffb1a71d2ba1f8b6c4b6ff0d41
nodzy <malenacorl@gmail.com>
2016-07-06 16:29:10 +0200

    Update warehouse.rb

--- warehouse.rb
+++ warehouse.rb
@@ -3,9 +3,9 @@ class Warehouse
   attr_accessor :city, :employees, :products
 
   def initialize(city:, employees:)
-  	@city = city
-  	@employees = employees
-  	@products = []
+    @city = city
+    @employees = employees
+    @products = []
   end 
 
   def add(product)
@@ -15,20 +15,20 @@ class Warehouse
   def remove(product)
     if !products.empty?
 	  products.delete_at(products.
-      find_index{|p| p.id == product.id})
+            find_index{|p| p.id == product.id})
     end
   end
 
   def print_stock
-  	puts "\n\tCity:\t#{city}\t\tName:\t\tPrice:\t\tQty:"
-	products.sort{|a,b| a.name <=> b.name}.
-	  uniq.each do |p|
-	    puts "\t\t\t\t#{p.name}\t\t#{Product.price_formatted(p.price)}\t\t#{@products.select{|e| e.name == p.name}.count}"
-	end
-	puts "\n\tSum: #{Product.price_formatted(sum_in_stock)}\n"
-	puts "\tSum with VAT: #{Product.price_formatted(final_sum_in_stock)}\n"
-	puts "\n\t #{products.select{|p| p.sale == true}.
-	  uniq.map{|p| p.name}} on sale!" if products.select{|p| p.sale == true}.any?  
+    puts "\n\tCity:\t#{city}\t\tName:\t\tPrice:\t\tQty:"
+    products.sort{|a,b| a.name <=> b.name}.
+      uniq.each do |p|
+        puts "\t\t\t\t#{p.name}\t\t#{Product.price_formatted(p.price)}\t\t#{@products.select{|e| e.name == p.name}.count}"
+      end
+    puts "\n\tSum: #{Product.price_formatted(sum_in_stock)}\n"
+    puts "\tSum with VAT: #{Product.price_formatted(final_sum_in_stock)}\n"
+    puts "\n\t #{products.select{|p| p.sale == true}.
+      uniq.map{|p| p.name}} on sale!" if products.select{|p| p.sale == true}.any?  
   end
 
   def sum_in_stock
